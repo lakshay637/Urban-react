9f8cbbc29d1e78035b8aa28f28fb4fbf3ff2ffba
lakshay637 <lakshayb211@gmail.com>
2025-07-03 16:29:36 +0530

    first commit

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,29 +1,36 @@
 import React from "react";
 import PropTypes from 'prop-types';
+import { Link } from "react-router-dom";
 
 
 
 export default function Navbar(props) {
     return (
         <div>
-            <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-                <a className="navbar-brand" href="/"> {props.title}</a>
+            <nav className={`navbar navbar-expand-lg navbar-${props.mode} bg-${props.mode}`}>
+                <div className="container-fluid">
+                <Link className="navbar-brand" to="/"> {props.title}</Link>
                 <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul className="navbar-nav mr-auto">
                         <li className="nav-item active">
-                            <a className="nav-link" href="/"> Home <span className="sr-only">(current)</span> </a>
+                            <Link className="nav-link" to="/"> Home</Link>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link" href="/">{props.aboutText} </a>
+                            <Link className="nav-link" to="/about">{props.aboutText} </Link>
                         </li>
                     </ul>
-                    <form className="form-inline my-2 my-lg-0">
+                    </div>
+                    {/*<form className="form-inline my-2 my-lg-0">
                         <input className="form-control mr-sm-2" type="search" placeholder="Search"aria-label="Search" />
                         <button className="btn btn-outline-primary" type="submit">Search</button>
-                    </form>
+                    </form>*/}
+                    <div className={`form-check form-switch text-${props.mode==='light'?'dark':'light'}`}>
+                        <input className="form-check-input" onClick={props.toggleMode} type="checkbox" role="switch" id="switchCheckDefault"/>
+                        <label className="form-check-label" htmlFor="switchCheckDefault">Enable DarkMode </label>
+                    </div>
                 </div>
             </nav>
         </div>
@@ -35,7 +42,3 @@ Navbar.propTypes = {
     aboutText: PropTypes.string
 };
 
-//Navbar.defaultProps = {
-//    title: 'Urban Clone',
-//    aboutText: 'About Urban'
-//};
